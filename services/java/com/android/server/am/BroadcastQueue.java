3839668b5c183b018cf0934763af9998767e712e
nitroglycerine33 <nitroglycerine33@gmail.com>
2013-09-03 16:58:14 -0500

    Adjust activity manager broadcasts for framework_support app calls

--- services/java/com/android/server/am/BroadcastQueue.java
+++ services/java/com/android/server/am/BroadcastQueue.java
@@ -21,6 +21,7 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import android.app.ActivityManager;
+import android.app.ActivityManagerNative;
 import android.app.AppGlobals;
 import android.app.AppOpsManager;
 import android.content.ComponentName;
@@ -118,6 +119,11 @@ public class BroadcastQueue {
      * waiting for this one.
      */
     BroadcastRecord mPendingBroadcast = null;
+
+    /**
+     * Intent broadcast that we are currently processing
+     */
+    BroadcastRecord mCurrentBroadcast = null;
 
     /**
      * The receiver index that is pending, to restart the broadcast if needed.
@@ -466,6 +472,10 @@ public class BroadcastQueue {
         }
     }
 
+    BroadcastRecord getProcessingBroadcast() {
+        return mCurrentBroadcast;
+    }
+
     final void processNextBroadcast(boolean fromMsg) {
         synchronized(mService) {
             BroadcastRecord r;
@@ -486,6 +496,7 @@ public class BroadcastQueue {
                 r = mParallelBroadcasts.remove(0);
                 r.dispatchTime = SystemClock.uptimeMillis();
                 r.dispatchClockTime = System.currentTimeMillis();
+                mCurrentBroadcast = r;
                 final int N = r.receivers.size();
                 if (DEBUG_BROADCAST_LIGHT) Slog.v(TAG, "Processing parallel broadcast ["
                         + mQueueName + "] " + r);
@@ -497,6 +508,7 @@ public class BroadcastQueue {
                     deliverToRegisteredReceiverLocked(r, (BroadcastFilter)target, false);
                 }
                 addBroadcastToHistoryLocked(r);
+                mCurrentBroadcast = null;
                 if (DEBUG_BROADCAST_LIGHT) Slog.v(TAG, "Done with parallel broadcast ["
                         + mQueueName + "] " + r);
             }
@@ -546,6 +558,7 @@ public class BroadcastQueue {
                     return;
                 }
                 r = mOrderedBroadcasts.get(0);
+                mCurrentBroadcast = r;
                 boolean forceReceive = false;
 
                 // Ensure that even if something goes awry with the timeout
@@ -618,6 +631,7 @@ public class BroadcastQueue {
                     // ... and on to the next...
                     addBroadcastToHistoryLocked(r);
                     mOrderedBroadcasts.remove(0);
+                    mCurrentBroadcast = null;
                     r = null;
                     looped = true;
                     continue;
