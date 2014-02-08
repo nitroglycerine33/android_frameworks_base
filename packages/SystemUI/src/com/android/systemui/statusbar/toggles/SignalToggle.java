AOKP/frameworks_base

SystemUI: toggle enhancements
Provide a default icon with each toggle
so that ROM Control can query these drawables
and display them to be prettier!

Change-Id: I9fe0b860dd7d9b7d8ce8fc5d1dcfa9186f809fb0
Signed-off-by: Roman Birg <romanbirg@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/toggles/SignalToggle.java
@@ -166,4 +166,8 @@ public class SignalToggle extends StatefulToggle implements NetworkSignalChanged
         scheduleViewUpdate();
     }
 
+    @Override
+    public int getDefaultIconResId() {
+        return R.drawable.ic_qs_signal_full_4g;
+    }
 }

nitroglycerine33/android_frameworks_base

Conflicts:
	packages/SystemUI/src/com/android/systemui/statusbar/toggles/QuietHoursToggle.java

packages/SystemUI/src/com/android/systemui/statusbar/toggles/SignalToggle.java
@@ -166,4 +166,8 @@ public class SignalToggle extends StatefulToggle implements NetworkSignalChanged
         scheduleViewUpdate();
     }
 
+    @Override
+    public int getDefaultIconResId() {
+        return R.drawable.ic_qs_signal_full_3;
+    }
 }